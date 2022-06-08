906b7d3526e6e2a9894dd0cb1061881364d28111
Vishal kumar <vishalk80269@gmail.com>
2022-06-08 18:20:40 +0530

    add smooth down navbar

--- JSX/menu.js
+++ JSX/menu.js
@@ -7,10 +7,12 @@ function tryDo(){
         valsZ=true;
     document.getElementById("lasty").style.top='0px';
     document.getElementById("closed").innerHTML='CLOSE';
+    // document.getElementById("oneLavels").style.display='block';
      }else{
          valsZ=false;
      document.getElementById("lasty").style.top='-1200px';
      document.getElementById("closed").innerHTML='MENU';
+    //  document.getElementById("oneLavels").style.display='none';
    
 }
 }
