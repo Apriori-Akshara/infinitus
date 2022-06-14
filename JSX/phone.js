1d425cd5e382b36c7830a9c2067fb78fcd54588e
Vishal kumar <vishalk80269@gmail.com>
2022-06-15 04:36:09 +0530

    only footer part is left

---
+++ JSX/phone.js
@@ -0,0 +1,504 @@
+const phone=[
+    {
+        id:"1",
+        country:"U.S. and Canada",
+        contact1:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"1",
+                contact1:"+1 877 683 0497 (English)",
+                
+
+            },
+            {
+                id:"2",
+                contact1:"+1 857 829 5064 (Spanish)",
+            }
+        ],
+       
+    },
+      {
+        id:"2",
+        country:"Brazil",
+        contact1:"+55 21 2018 1092 (Portuguese)",
+        phones:[
+            {
+                id:"2",
+                contact1:"+55 21 2018 1092 (Portuguese)",
+                
+
+            }
+        ],
+       
+    },
+    {
+        id:"3",
+        country:"Chile",
+        contact1:"+56 2 2582 3853 (Spanish)",
+        phones:[
+            {
+                id:"3",
+                contact1:"+56 2 2582 3853 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    {
+        id:"4",
+        country:"Colombia",
+        contact1:"+57 601 381 9242 (Spanish)",
+        phones:[
+            {
+                id:"4",
+                contact1:"+57 601 381 9242 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    {
+        id:"5",
+        country:"Mexico",
+        contact1:"+52 55 8525 9498 (Spanish)",
+        phones:[
+            {
+                id:"5",
+                contact1:"+52 55 8525 9498 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    
+]
+
+
+const europe=[
+    {
+        id:"1",
+        country:"U.S. and Canada",
+        contact1:"+1 877 683 0497 (English)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"1",
+                contact1:"+1 877 683 0497 (English)",
+                
+
+            },
+            {
+                id:"2",
+                contact1:"+1 857 829 5064 (Spanish)",
+            }
+        ],
+       
+    },
+      {
+        id:"2",
+        country:"Brazil",
+        contact1:"+55 21 2018 1092 (Portuguese)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"2",
+                contact1:"+55 21 2018 1092 (Portuguese)",
+                
+
+            }
+        ],
+       
+    },
+    {
+        id:"3",
+        country:"Chile",
+        contact1:"+56 2 2582 3853 (Spanish)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"3",
+                contact1:"+56 2 2582 3853 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    {
+        id:"4",
+        country:"Colombia",
+        contact1:"+57 601 381 9242 (Spanish)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"4",
+                contact1:"+57 601 381 9242 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    {
+        id:"5",
+        country:"Mexico",
+        contact1:"+52 55 8525 9498 (Spanish)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"5",
+                contact1:"+52 55 8525 9498 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+
+    {
+        id:"7",
+        country:"U.S. and Canada",
+        contact1:"+1 877 683 0497 (English)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"7",
+                contact1:"+1 877 683 0497 (English)",
+                
+
+            },
+            {
+                id:"7",
+                contact1:"+1 857 829 5064 (Spanish)",
+                contact2:"+1 877 683 0497 (English)",
+            }
+        ],
+       
+    },
+      {
+        id:"8",
+        country:"Brazil",
+        contact1:"+55 21 2018 1092 (Portuguese)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"8",
+                contact1:"+55 21 2018 1092 (Portuguese)",
+                
+
+            }
+        ],
+       
+    },
+    {
+        id:"9",
+        country:"Chile",
+        contact1:"+56 2 2582 3853 (Spanish)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"9",
+                contact1:"+56 2 2582 3853 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    {
+        id:"10",
+        country:"Colombia",
+        contact1:"+57 601 381 9242 (Spanish)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"10",
+                contact1:"+57 601 381 9242 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    {
+        id:"11",
+        country:"Mexico",
+        contact1:"+52 55 8525 9498 (Spanish)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"11",
+                contact1:"+52 55 8525 9498 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    {
+        id:"12",
+        country:"Mexico",
+        contact1:"+52 55 8525 9498 (Spanish)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"12",
+                contact1:"+52 55 8525 9498 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+
+    {
+        id:"13",
+        country:"U.S. and Canada",
+        contact1:"+1 877 683 0497 (English)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"13",
+                contact1:"+1 877 683 0497 (English)",
+                
+
+            },
+            {
+                id:"13",
+                contact1:"+1 857 829 5064 (Spanish)",
+            }
+        ],
+       
+    },
+      {
+        id:"14",
+        country:"Brazil",
+        contact1:"+55 21 2018 1092 (Portuguese)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"14",
+                contact1:"+55 21 2018 1092 (Portuguese)",
+                
+
+            }
+        ],
+       
+    },
+    {
+        id:"15",
+        country:"Chile",
+        contact1:"+56 2 2582 3853 (Spanish)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"15",
+                contact1:"+56 2 2582 3853 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    {
+        id:"16",
+        country:"Chile",
+        contact1:"+56 2 2582 3853 (Spanish)",
+        contact2:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"16",
+                contact1:"+56 2 2582 3853 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+  
+  
+  
+    
+]
+
+
+
+const asia=[
+    {
+        id:"1",
+        country:"U.S. and Canada",
+        contact1:"+1 877 683 0497 (English)",
+        phones:[
+            {
+                id:"1",
+                contact1:"+1 877 683 0497 (English)",
+                
+
+            },
+            {
+                id:"2",
+                contact1:"+1 857 829 5064 (Spanish)",
+            }
+        ],
+       
+    },
+      {
+        id:"2",
+        country:"Brazil",
+        contact1:"+55 21 2018 1092 (Portuguese)",
+        phones:[
+            {
+                id:"2",
+                contact1:"+55 21 2018 1092 (Portuguese)",
+                
+
+            }
+        ],
+       
+    },
+    {
+        id:"3",
+        country:"Chile",
+        contact1:"+56 2 2582 3853 (Spanish)",
+        phones:[
+            {
+                id:"3",
+                contact1:"+56 2 2582 3853 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    {
+        id:"4",
+        country:"Colombia",
+        contact1:"+57 601 381 9242 (Spanish)",
+        phones:[
+            {
+                id:"4",
+                contact1:"+57 601 381 9242 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    {
+        id:"5",
+        country:"Mexico",
+        contact1:"+52 55 8525 9498 (Spanish)",
+        phones:[
+            {
+                id:"5",
+                contact1:"+52 55 8525 9498 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    {
+        id:"6",
+        country:"Colombia",
+        contact1:"+57 601 381 9242 (Spanish)",
+        phones:[
+            {
+                id:"6",
+                contact1:"+57 601 381 9242 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    {
+        id:"7",
+        country:"Mexico",
+        contact1:"+52 55 8525 9498 (Spanish)",
+        phones:[
+            {
+                id:"7",
+                contact1:"+52 55 8525 9498 (Spanish)",
+               
+
+            }
+        ],
+       
+    },
+    
+]
+
+
+// america map function
+
+const dataJ=document.getElementById("phonedata");
+phone.map((item,index)=>(
+    dataJ.innerHTML += `
+    <div class="col-md-4 phone my-5" key=${item}>
+            <div class="row">
+                        <h3>
+                            ${item.country}
+                        </h3>
+                    </div>
+                    <div class="row mt-3">
+                        <p onClick='call(${item.contact1})'>
+                        ${item.contact1}
+                        </p>
+                    </div>
+                    </div>
+      `
+))
+
+
+// europe map function
+
+
+const dataI=document.getElementById("phonedataEuro");
+europe.map((item,index)=>(
+    dataI.innerHTML += `
+    <div class="col-md-3 phone my-5" key=${item}>
+            <div class="row">
+                        <h3>
+                            ${item.country}
+                        </h3>
+                    </div>
+                    <div class="row mt-3">
+                        <p onClick='call(${item.contact1})'>
+                        ${item.contact1}
+                        </p>
+                    </div>
+                    <div class="row mt-3">
+                        <p onClick='call(${item.contact2})'>
+                        ${item.contact1}
+                        </p>
+                    </div>
+                    </div>
+      `
+))
+
+
+
+
+// Asia Pacific
+const dataG=document.getElementById("phonedataAsia");
+asia.map((item,index)=>(
+    dataG.innerHTML += `
+    <div class="col-md-3 phone my-5" key=${item}>
+            <div class="row">
+                        <h3>
+                            ${item.country}
+                        </h3>
+                    </div>
+                    <div class="row mt-3">
+                        <p onClick='call(${item.contact1})'>
+                        ${item.contact1}
+                        </p>
+                    </div>
+                   
+                    </div>
+      `
+))
